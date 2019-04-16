31d56fa49aaa233241e51ca67809111ba4c5c4ba
Barış Ekim <ekimb@mit.edu>
2019-04-16 11:45:01 -0400

    benchmark parsing

--- pasha.cpp
+++ pasha.cpp
@@ -1,10 +1,10 @@
 /**
-    Pasha: Parallel Algorithms for Approximating Compact Universal Hitting Sets
-    pasha.cpp
-    Main file for parsing arguments, running decycling set calculations, and
-    outputting hitting sets.
-    @author Baris Ekim
-    @version 1.0 4/15/19
+Pasha: Parallel Algorithms for Approximating Compact Universal Hitting Sets
+pasha.cpp
+Main file for parsing arguments, running decycling set calculations, and
+outputting hitting sets.
+@author Baris Ekim
+@version 1.0 4/15/19
 */
 #include <iostream>
 #include <sys/resource.h> 
@@ -29,6 +29,21 @@
 #define ANSI_COLOR_MAGENTA "\x1b[35m"
 #define ANSI_COLOR_CYAN    "\x1b[36m"
 #define ANSI_COLOR_RESET   "\x1b[0m"
+
+void printBenchmarkHelp(string argError) {
+/**
+Prints help log for benchmarks.
+@param argError: Error message.
+*/
+    cout << "Usage: pasha " << BENCHMARK << " -k <kmerlength> -l <seqlength>" << endl;
+    cout << "-k <kmerlength [int 5-12]> k-mer length (required)." << endl;
+    cout << "-l <seqlength [int 20-200]> sequence length (required)." << endl;
+    if (argError != "") {
+        cout << "Input Error: "; printf(ANSI_COLOR_RED "%s" ANSI_COLOR_RESET "\n", argError.c_str());
+        cout << "Please follow the specifications above." << endl << endl;
+    }
+    exit(0);
+}
 
 void printDecyclingHelp(string argError) {
 /**
@@ -61,21 +76,30 @@ Prints help log for hitting set.
     }
     exit(0);
 }
-void printBenchmarkHelp(string argError) {
-/**
-Prints help log for benchmarks.
-@param argError: Error message.
-*/
-    cout << "Usage: pasha " << BENCHMARK << " -k <kmerlength> -l <seqlength>" << endl;
-    cout << "-k <kmerlength [int 5-12]> k-mer length (required)." << endl;
-    cout << "-l <seqlength [int 20-200]> sequence length (required)." << endl;
-    if (argError != "") {
-        cout << "Input Error: "; printf(ANSI_COLOR_RED "%s" ANSI_COLOR_RESET "\n", argError.c_str());
-        cout << "Please follow the specifications above." << endl << endl;
-    }
-    exit(0);
-}
 
+
+graph regenerateGraph(int k, string decyclingFile) {
+/**
+Regenerates graph and removes the decycling set for benchmarking purposes.
+@param k: K-mer length, decyclingFile: Decycling output file destination.
+@return newGraph: Resulting graph.
+*/  
+    ofstream decyclingStream;
+    graph newGraph = graph(k);
+    decycling newDecycling;
+    vector<int> decyclingSet = newDecycling.computeDecyclingSet(k);
+    decyclingFile = "pasha_" + to_string(k) + "/" + decyclingFile;
+    decyclingStream.open(decyclingFile);
+    for (int i = 0; i < decyclingSet.size(); i++) {
+        string label = newGraph.getLabel(decyclingSet[i]);
+        newGraph.removeEdge(decyclingSet[i]);
+        decyclingStream << label << "\n";
+    }
+    int decyclingSize = decyclingSet.size();
+    cout << "Decycling set size: " << decyclingSize << endl;
+    decyclingStream.close();
+    return newGraph;
+}
 
 int main(int argc, char* argv[]) {
     int k = 0;
@@ -92,14 +116,12 @@ int main(int argc, char* argv[]) {
     const double PI = 3.14159;
     string directory;
     const string ALPHABET = "ACGT";
-    ofstream decyclingStream;
-    ofstream hittingStream;
-    cout << endl;
     if (argc == 1 || string(argv[1]) == "-h" || string(argv[1]) == "--help" || string(argv[1]) == "help") {
         cout << "Usage: pasha <command> [options]" << endl << endl;
         cout << "Command: " << DECYCLING << "\t Calculate only the decycling set for k-mer length k." << endl;
         cout << "         " << GENERATE << "\t Calculate the hitting sets using the options specified (the decycling set will automatically be generated)." << endl;
-        cout << endl << "For help on either command, run 'pasha <command>' without any options." << endl << endl;
+        cout << "         " << BENCHMARK << "\t Calculate hitting sets using all of the different methods for benchmarking." << endl;
+        cout << endl << "For help on any command, run 'pasha <command>' without any options." << endl << endl;
         exit(0);
     }
     string argFirst = string(argv[1]);
@@ -113,9 +135,9 @@ int main(int argc, char* argv[]) {
                     char *end;
                     k = strtol(argv[i+1], &end, 10);
                     if (*end != '\0') printDecyclingHelp("Non-integer or unspecified k-mer length.");
+                    if (k < 5 || k > 12) printDecyclingHelp("Pasha only supports k-mer lengths between 5 and 12.");
                     directory = "pasha_" + string(argv[i+1]);
                     decyclingFile = "pasha_" + string(argv[i+1]) + "/d" + string(argv[i+1]) + ".txt";
-                    if (k < 5 || k > 12) printDecyclingHelp("Pasha only supports k-mer lengths between 5 and 12.");
                     i += 2;
                 }
                 else printDecyclingHelp("Incorrect argument or value " + argNext+ "."); 
@@ -153,9 +175,9 @@ int main(int argc, char* argv[]) {
                     char *end;
                     k = strtol(argv[i+1], &end, 10);
                     if (*end != '\0') printGenerateHelp("Non-integer or unspecified k-mer length.");
+                    if (k < 5 || k > 12) printGenerateHelp("Pasha only supports k-mer lengths between 5 and 12.");
                     directory = "pasha_" + string(argv[i+1]);
                     decyclingFile = "pasha_" + string(argv[i+1]) + "/d" + string(argv[i+1]) + ".txt";
-                    if (k < 5 || k > 12) printGenerateHelp("Pasha only supports k-mer lengths between 5 and 12.");
                     i += 2;
                 } 
                 else if (argNext == "-l") {
@@ -163,9 +185,8 @@ int main(int argc, char* argv[]) {
                     char *end;
                     L = strtol(argv[i+1], &end, 10);
                     if (*end != '\0') printGenerateHelp("Non-integer or unspecified sequence length.");
-                    directory = "pasha_" + string(argv[i+1]);
-                    decyclingFile = "pasha_" + string(argv[i+1]) + "/d" + string(argv[i+1]) + ".txt";
                     if (L < 20 || L > 200) printGenerateHelp("Pasha only supports sequence lengths between 20 and 200.");
+                    hittingFile =  to_string(k) + string(argv[i+1]);
                     i += 2;
                 }
                 else printGenerateHelp("Incorrect argument or value " + argNext+ ".");      
@@ -179,40 +200,38 @@ int main(int argc, char* argv[]) {
         else {
             for (int i = 2; i < argc;) {
                 string argNext = string(argv[i]);
-                if (argNext == "-k") {
-                    directory = "pasha_" + string(argv[i+1]);
-                    k = atoi(argv[i+1]);
+                if (argc != 6) printBenchmarkHelp("Incorrect number of arguments or values.");
+                else if (argNext == "-k") {
+                    kmerLen = true;
+                    char *end;
+                    k = strtol(argv[i+1], &end, 10);
+                    if (*end != '\0') printBenchmarkHelp("Non-integer or unspecified k-mer length.");
                     if (k < 5 || k > 12) printBenchmarkHelp("Pasha only supports k-mer lengths between 5 and 12.");
+                    directory = "pasha_" + string(argv[i+1]);
+                    decyclingFile = "pasha_" + string(argv[i+1]) + "/d" + string(argv[i+1]) + ".txt";
                     i += 2;
-                }
+                } 
                 else if (argNext == "-l") {
-                    L = atoi(argv[i+1]);
-                    hittingFile =  to_string(k) + string(argv[i+1]);
+                    seqLen = true;
+                    char *end;
+                    L = strtol(argv[i+1], &end, 10);
+                    if (*end != '\0') printBenchmarkHelp("Non-integer or unspecified sequence length.");
                     if (L < 20 || L > 200) printBenchmarkHelp("Pasha only supports sequence lengths between 20 and 200.");
+                    hittingFile =  to_string(k) + string(argv[i+1]);
                     i += 2;
                 }
-                else printDecyclingHelp("Incorrect argument or value " + argNext+ ".");
+                else printBenchmarkHelp("Incorrect argument or value " + argNext+ ".");      
             }
+            if (kmerLen == false) printBenchmarkHelp("Please provide a k-mer length.");
+            if (seqLen == false) printBenchmarkHelp("Please provide a sequence length.");
         }
     }
-
+    graph newGraph = regenerateGraph(k, decyclingFile);
+    int hittingSize = 0;
     if (mkdir(directory.c_str(), 0777) == -1) cout << strerror(errno) << "." << endl; 
     else cout << "Directory created." << endl; 
     if (argFirst == DECYCLING) cout << "Decycling set will be saved to: " << decyclingFile << endl; 
-    graph newGraph = graph(k);
-    decycling newDecycling;
-    vector<int> decyclingSet = newDecycling.computeDecyclingSet(k);
-    decyclingFile = "pasha_" + to_string(k) + "/" + decyclingFile;
-    decyclingStream.open(decyclingFile);
-    for (int i = 0; i < decyclingSet.size(); i++) {
-        string label = newGraph.getLabel(decyclingSet[i]);
-        newGraph.removeEdge(decyclingSet[i]);
-        decyclingStream << label << "\n";
-    }
-    int decyclingSize = decyclingSet.size();
-    decyclingStream.close();
-    int hittingSize = 0;
-    if (argFirst == GENERATE) {
+    else if (argFirst == GENERATE) {
         struct timespec start, finish;
         double elapsed;
         clock_gettime(CLOCK_MONOTONIC, &start);  
@@ -262,7 +281,76 @@ int main(int argc, char* argv[]) {
         elapsed += (finish.tv_nsec - start.tv_nsec) / 1000000000.0;
         cout << elapsed << " seconds." << endl;
     }
+    else if (argFirst == BENCHMARK) {
+        hittingFile = "pasha_" + to_string(k) + "/" + hittingFile;
+        cout << "Decycling set will be saved to: " << decyclingFile << endl;
+        cout << "Hitting sets will be saved to: " << hittingFile + "*.txt" << endl << endl;
+        struct timespec start, finish;
+        double elapsed;
+        cout << hittingFile + ".txt:" << endl;
+        clock_gettime(CLOCK_MONOTONIC, &start);
+        hittingSize = newGraph.Hitting(L, (hittingFile + ".txt"));
+        clock_gettime(CLOCK_MONOTONIC, &finish);
+        elapsed = (finish.tv_sec - start.tv_sec);
+        elapsed += (finish.tv_nsec - start.tv_nsec) / 1000000000.0;
+        cout << elapsed << " seconds." << endl; 
+        cout << "Hitting set size: " << hittingSize << endl << endl;
+        newGraph = regenerateGraph(k, decyclingFile);
+        cout << hittingFile + "p.txt:" << endl;
+        clock_gettime(CLOCK_MONOTONIC, &start);
+        hittingSize = newGraph.HittingParallel(L, (hittingFile + "p.txt"));
+        clock_gettime(CLOCK_MONOTONIC, &finish);
+        elapsed = (finish.tv_sec - start.tv_sec);
+        elapsed += (finish.tv_nsec - start.tv_nsec) / 1000000000.0;
+        cout << elapsed << " seconds." << endl; 
+        cout << "Hitting set size: " << hittingSize << endl << endl;
+        newGraph = regenerateGraph(k, decyclingFile);
+        cout << hittingFile + "a.txt:" << endl;
+        clock_gettime(CLOCK_MONOTONIC, &start);
+        hittingSize = newGraph.HittingAny(L, 1, (hittingFile + "a.txt"));
+        clock_gettime(CLOCK_MONOTONIC, &finish);
+        elapsed = (finish.tv_sec - start.tv_sec);
+        elapsed += (finish.tv_nsec - start.tv_nsec) / 1000000000.0;
+        cout << elapsed << " seconds." << endl; 
+        cout << "Hitting set size: " << hittingSize << endl << endl;
+        newGraph = regenerateGraph(k, decyclingFile);
+        cout << hittingFile + "a625.txt:" << endl;
+        clock_gettime(CLOCK_MONOTONIC, &start);
+        hittingSize = newGraph.HittingAny(L, 625, (hittingFile + "a625.txt"));
+        clock_gettime(CLOCK_MONOTONIC, &finish);
+        elapsed = (finish.tv_sec - start.tv_sec);
+        elapsed += (finish.tv_nsec - start.tv_nsec) / 1000000000.0;
+        cout << elapsed << " seconds." << endl; 
+        cout << "Hitting set size: " << hittingSize << endl << endl;
+        newGraph = regenerateGraph(k, decyclingFile);
+        cout << hittingFile + "ap.txt:" << endl;
+        clock_gettime(CLOCK_MONOTONIC, &start);
+        hittingSize = newGraph.HittingParallelAny(L, 1, (hittingFile + "ap.txt"));
+        clock_gettime(CLOCK_MONOTONIC, &finish);
+        elapsed = (finish.tv_sec - start.tv_sec);
+        elapsed += (finish.tv_nsec - start.tv_nsec) / 1000000000.0;
+        cout << elapsed << " seconds." << endl; 
+        cout << "Hitting set size: " << hittingSize << endl << endl;
+        newGraph = regenerateGraph(k, decyclingFile);
+        cout << hittingFile + "ap625.txt:" << endl;
+        clock_gettime(CLOCK_MONOTONIC, &start);
+        hittingSize = newGraph.HittingParallelAny(L, 625, (hittingFile + "ap625.txt"));
+        clock_gettime(CLOCK_MONOTONIC, &finish);
+        elapsed = (finish.tv_sec - start.tv_sec);
+        elapsed += (finish.tv_nsec - start.tv_nsec) / 1000000000.0;
+        cout << elapsed << " seconds." << endl; 
+        cout << "Hitting set size: " << hittingSize << endl << endl;
+        newGraph = regenerateGraph(k, decyclingFile);
+        cout << hittingFile + "r.txt:" << endl;
+        clock_gettime(CLOCK_MONOTONIC, &start);
+        hittingSize = newGraph.HittingRandomParallel(L, (hittingFile + "r.txt"));
+        clock_gettime(CLOCK_MONOTONIC, &finish);
+        elapsed = (finish.tv_sec - start.tv_sec);
+        elapsed += (finish.tv_nsec - start.tv_nsec) / 1000000000.0;
+        cout << elapsed << " seconds." << endl; 
+        cout << "Hitting set size: " << hittingSize << endl << endl;
+        cout << "All done!" << endl;
+    }
     if (argFirst == GENERATE) cout << "Hitting set size: " << hittingSize << endl;
-    if (argFirst == DECYCLING) cout << "Decycling set size: " << decyclingSize << endl;
-    return hittingSize + decyclingSize;
+    return 0;
 }
