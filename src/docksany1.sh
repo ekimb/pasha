0e0e1450a9c4c2dbf0084a30b21b29779614a9cc
Barış Ekim <ekimb@mit.edu>
2019-07-10 10:40:00 +0300

    Update docksany1.sh

--- src/docksany1.sh
+++ src/docksany1.sh
@@ -1,2 +1,2 @@
 touch ../bench/DOCKSbench$1$2$3.txt
-command time -v ./docks $1 $2 $3 decyc$1.txt DOCKSany$1_$2_$3.txt 2>&1 | tee -a ../bench/DOCKSanybench$1$2$3.txt
+command time -v ./docksany $1 $2 $3 decyc$1.txt DOCKSany$1_$2_$3.txt 2>&1 | tee -a ../bench/DOCKSanybench$1$2$3.txt
