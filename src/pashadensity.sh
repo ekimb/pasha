138be1ba8d43f3457c64c6cf17a5a85e0f7a81ae
Barış Ekim <ekimb@mit.edu>
2019-07-30 14:53:36 +0300

    Update pashadensity.sh

--- src/pashadensity.sh
+++ src/pashadensity.sh
@@ -1,2 +1,2 @@
 touch ../bench/PASHAdensity$2$1.txt
-command time -v ./density $1 $2 $3 PASHA$1_$2.txt decyc$1.txt  2>&1 | tee -a ../bench/PASHAdensity$2$1.txt
+command time -v ./density $1 $2 $3 PASHA$2_$3.txt decyc$2.txt  2>&1 | tee -a ../bench/PASHAdensity$2$1.txt
